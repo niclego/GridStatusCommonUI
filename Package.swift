19c6ea124cc0d68104cdd680aeff8d80463d8596
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2023-04-25 08:26:19 -0400

    Set up

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "grid_status_common_ui",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "grid_status_common_ui",
+            targets: ["grid_status_common_ui"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "grid_status_common_ui"),
+        .testTarget(
+            name: "grid_status_common_uiTests",
+            dependencies: ["grid_status_common_ui"]),
+    ]
+)
---
+++ Tests/grid_status_common_uiTests/grid_status_common_uiTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import grid_status_common_ui
+
+final class grid_status_common_uiTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
