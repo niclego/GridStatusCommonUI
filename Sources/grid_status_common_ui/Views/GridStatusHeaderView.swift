37dad7ef38838ed50d568cdcb09538c8e151c927
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2023-04-25 09:40:29 -0400

    more ui

--- Sources/grid_status_common_ui/Views/GridStatusHeaderView.swift
+++ Sources/grid_status_common_ui/Views/GridStatusHeaderView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 public struct GridStatusHeaderView: View {
     @Environment(\.colorScheme) var colorScheme
+    
+    public init() {}
 
     public var body: some View {
         Text("Gridstatus.io").font(.title)
