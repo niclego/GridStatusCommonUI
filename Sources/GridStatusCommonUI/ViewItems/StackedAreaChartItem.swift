294370230e2ef972e57389104f0c868caf63049c
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2023-05-03 17:18:17 -0400

    dev...

--- Sources/GridStatusCommonUI/ViewItems/StackedAreaChartItem.swift
+++ Sources/GridStatusCommonUI/ViewItems/StackedAreaChartItem.swift
@@ -64,12 +64,12 @@ extension StackedAreaChartItem {
     
     static let examples: [StackedAreaChartItem] = {
         let calendar = Calendar.current
-        let startDate = calendar.date(byAdding: .minute, value: 5, to: calendar.startOfDay(for: Date.now))!
+        let startDate = calendar.startOfDay(for: Date.now)
         let formatter = DateFormatter()
         
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         
-        let items = (0...286).map { i in
+        let items = (0...287).map { i in
             let date = calendar.date(byAdding: .minute, value: 5 * i, to: startDate)!
             let timeStr = formatter.string(from: date)
             return StackedAreaChartItem(
